15d428ef7ef04cc55ae2721eda6088df12918329
fgaensslen <florian.gaensslen@googlemail.com>
2026-03-29 18:14:15 +0200

    Ignore changes in files folder to prevent restart

--- Website/start.sh
+++ Website/start.sh
@@ -5,4 +5,4 @@ nvm use 20
 cd /home/admin/ZenTable/Website
 export NODE_ENV=production
 export NODE_OPTIONS="--openssl-legacy-provider --no-deprecation"
-/home/admin/ZenTable/Website/node_modules/.bin/nodemon server/app.js --exec /home/admin/ZenTable/Website/node_modules/.bin/babel-node
+/home/admin/ZenTable/Website/node_modules/.bin/nodemon server/app.js --exec /home/admin/ZenTable/Website/node_modules/.bin/babel-node --ignore 'files/*' --ignore 'playlists.json'

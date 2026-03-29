c7e7b4fe949012af43561436a22b6a5e576df04b
fgaensslen <florian.gaensslen@googlemail.com>
2026-03-29 17:41:00 +0200

    Add autostart script for systemd service

--- Website/start.sh
+++ Website/start.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-. "$HOME"/.nvm/nvm.sh
-npm run prod
+export NVM_DIR="/home/admin/.nvm"
+source "$NVM_DIR/nvm.sh"
+nvm use 20
+cd /home/admin/ZenTable/Website
+export NODE_ENV=production
+export NODE_OPTIONS="--openssl-legacy-provider --no-deprecation"
+/home/admin/ZenTable/Website/node_modules/.bin/nodemon server/app.js --exec /home/admin/ZenTable/Website/node_modules/.bin/babel-node
